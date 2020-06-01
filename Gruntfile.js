283babeb669088465f27bd227e6b78792c04ca2f
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2020-06-01 20:05:34 -0300

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,4 +5,14 @@ module.exports = function(grunt){
 
     });
 
+    // Load plugins
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-image');
+
 };
